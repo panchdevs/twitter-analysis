f0468853214de892e7e77aa9a0f160a465e925a4
abhijitkatiyar <abhijitkatiyar@gmail.com>
2015-03-12 20:38:16 +0530

    Clean tweets before inserting in collection

--- tweet_collector.py
+++ tweet_collector.py
@@ -11,13 +11,15 @@ ACCESS_TOKEN_SECRET = ""
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 
+LIMIT = None
+
 if __name__ == '__main__':
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     client = MongoClient(MONGO_HOST, MONGO_PORT)
     tweet_collection = client.TwitterAnalysis.tweets
-    collector = TweetCollector(tweet_collection, 5)
+    collector = TweetCollector(tweet_collection, LIMIT)
     stream = Stream(auth, collector)
     stream.sample(languages=["en"])
     print('Finished collecting tweets')
--- tweetcleaner.py
+++ tweetcleaner.py
@@ -1,38 +1,21 @@
-#Lower Case - Convert the tweets to lower case.
-#URLs - I don't intend to follow the short urls and determine the content of the site, so we can eliminate all of these URLs via regular expression matching or replace with generic word URL.
-#@username - we can eliminate "@username" via regex matching or replace it with generic word AT_USER.
-#hashtag - hash tags can give us some useful information, so it is useful to replace them with the exact same word without the hash. E.g. #nike replaced with 'nike'.
-#Punctuations and additional white spaces - remove punctuation at the start and ending of the tweets. E.g: ' the day is beautiful! ' replaced with 'the day is beautiful'. It is also helpful to replace multiple whitespaces with a single whitespace
-
 import re
 
-#start process_tweet
-def processTweet(tweet):
-    # process the tweets
-
-    #Convert to lower case
+def process_tweet(tweet):
     tweet = tweet.lower()
-    #Convert www.* or https?://* to URL
-    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
-    #Convert @username to AT_USER
-    tweet = re.sub('@[^\s]+','AT_USER',tweet)
-    #  \s Matches any whitespace character equivalent to [ \t\n\r\f\v]
-    #Remove additional white spaces
-    tweet = re.sub('[\s]+', ' ', tweet)
-    #Replace #word with word
+
+    # Convert www.* or https?://* to URL
+    tweet = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+
+    # Convert @username to AT_USER
+    tweet = re.sub(r'@[^\s]+', 'AT_USER', tweet)
+
+    # Remove additional white spaces
+    tweet = re.sub(r'[\s]+', ' ', tweet)
+
+    # Replace #word with word
     tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-    #trim
+
+    # trim
     tweet = tweet.strip('\'"')
+
     return tweet
-#end
-
-#Read the tweets one by one and process it
-fp = open('sampleTweets.txt', 'r')
-line = fp.readline()
-
-while line:
-    processedTweet = processTweet(line)
-    print(processedTweet)
-    line = fp.readline()
-#end loop
-fp.close()
