bb603b2ea5cf00a4542f686e4bea5988bdc30768
pbaisla <prashant_baisla@yahoo.com>
2015-04-06 17:44:28 +0530

    Save classifier to file

--- twitter-analysis.py
+++ twitter-analysis.py
@@ -1,4 +1,5 @@
 from sklearn import svm
+from sklearn.externals import joblib
 from tweet_sentiment import get_features
 from tweetcleaner import process_tweet
 from tweepy import OAuthHandler, Stream
@@ -7,7 +8,7 @@ from tweet_listener import TweetListener
 import csv
 
 if __name__ == '__main__':
-    with open('dataset.csv') as dataset_file:
+    with open('data/dataset.csv') as dataset_file:
         dataset = csv.reader(dataset_file)
         features = []
         classes = []
@@ -19,3 +20,4 @@ if __name__ == '__main__':
 
     classifier = svm.SVC()
     classifier.fit(features, classes)
+    joblib.dump(classifier, 'data/classifier.pkl')
