4f7be15367db01d4a87cc228b74e11ffe8aed04a
Vivek Agarwal <vivek17.agarwal@gmail.com>
2015-04-22 15:24:08 +0530

    added part of speech analysis

--- lib/tweet_sentiment.py
+++ lib/tweet_sentiment.py
@@ -1,4 +1,5 @@
 import re
+import nltk
 
 def get_features(tweet):
     sentiment_score, neg_words, pos_words = calculate_sentiment_score(tweet)
@@ -6,27 +7,37 @@ def get_features(tweet):
     return [sentiment_score, pos_words, neg_words, pos_emoticon, neg_emoticon]
 
 def calculate_sentiment_score(tweet):
+    pos={'PRP':1,'NN':1,'DT':2,'VBP':3,'JJ':4}
     d = {}
-    fl = open("data/words.txt", 'r')
-    for line in fl:
-        word, score = line.split("\t")
-        d[word] = float(score)
-    fl.close()
-    lst = tweet.split(' ')
+    with open("../data/words.txt", 'r') as fl:
+        for line in fl:
+            word, score = line.split("\t")
+            d[word] = float(score)
+    lst = nltk.word_tokenize(tweet)
+    tokens = nltk.pos_tag(lst)
     score = 0
     i = 0
-    l = len(lst)
-    pos = 0
-    neg = 0
+    l = len(tokens)
+    positive = 0
+    negative = 0
     while i < l:
-        if lst[i] in d:
-            score += d[lst[i]]
-            if d[lst[i]] < 5:
-                neg += 1
+        if(tokens[i][0] in d and tokens[i][1] in pos):
+            if(d[tokens[i][0]] < 5):
+                score += (d[tokens[i][0]]*pos[tokens[i][1]]*(-1))
             else:
-                pos += 1
+                score += d[tokens[i][0]]*pos[tokens[i][1]]
+        elif (tokens[i][0] in d):
+            if(d[tokens[i][0]] < 5):
+                score += (d[tokens[i][0]]*(-1))
+            else:
+                score += d[tokens[i][0]]
+        if(tokens[i][0] in d):
+            if(d[tokens[i][0]] < 5):
+                negative += 1
+            else:
+                positive += 1
         i += 1
-    return (score, neg, pos)
+    return (score, negative, positive)
 
 def calculate_emoticon_score(tweet):
     no_of_pos = len(re.findall("positive\d emoticon", tweet))
